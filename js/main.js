c9e4b94d0faaf9e3dfbbbdc7d42cdcbb5c1cd673
joao <jprcarvalho1@gmail.com>
2021-06-05 19:26:41 -0300

    Adiciona textos de Sobre, Skills

--- js/main.js
+++ js/main.js
@@ -1,20 +1,78 @@
 Vue.component("tab-home", {
     template: `
         <div>
-            <h1> Seja Bem-Vindo</h1>
+            <div>
+                <h1>Bem-Vindo</h1>
+                <p>Olá, esta é minha página pessoal.</p>
+                <p>Co-Fundador da <a href="codetta.tech" target=_blank>Codetta Tech</a></p>
+            </div>
+            <div class="my-5">
+                <h1>Experiências</h1>
+                <ul>
+                    <li>{{ cargos[0] }} e {{ cargos[1] }} da {{ empresa }}.</li>
+                </ul>
+            </div>
+            <div class="my-5">
+                <h1>Formação Acadêmica</h1>
+                <ul>
+                    <li v-for='academico in academicos'>Cursando {{ academico }}</li>
+                </ul>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Hard-Skills</h1>
+                    <ul>
+                        <li v-for='hard in hards'>{{ hard }}</li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <h1>Soft-Skills</h1>
+                    <ul>
+                        <li v-for='soft in softs'>{{ soft }}</li>
+                    </ul>
+                </div>
+            </div>
         </div>`,
+        data: function() {
+            return {
+                cargos: ['Desenvolvedor de Sistemas', 'co-CEO'],
+                empresa: "Codetta Tech",
+                academicos: ['Engenharia de Computação pela Universidade Estadual de Feira de Santana(UEFS)',
+                'Técnico em Desenvolvimento de Sistemas pelo SENAI-BA'],
+                hards: ['JavaScript', 'HTML', 'CSS', 'Java', 'Jekyll', 'Hugo', 'Vue'],
+                softs: ['Trabalho em Equipe', 'Capacidade adaptativa', 'Resiliência', 'Comunicação', 'Ética', 'Pensamento Crítico', 'Atitude Positiva']
+            }
+        }
 });
 
 Vue.component("tab-sobre", {
     template: `
-        <div>
-            <h1>Sobre</h1>
-            <p> Me chamo {{ nome }}, tenho {{ idade }} anos de idade.</p>
+        <div class="row">
+            <div class="col-md-8">
+                <h1>Sobre</h1>
+                <p> O meu nome é {{ nome }}, tenho {{ idade }} anos de idade, solteiro, 
+                nasci e ainda moro na cidade de {{ cidade }} na {{ estado }}. 
+                Sou estudante de {{ cursos[0] }} e de {{ cursos[1] }}.<br>
+                Também fundei em 2021, junto com um amigo, a 
+                <a href="https://codetta.tech" target=_blank>Codetta Tech</a>, 
+                uma Software House com o objetivo de proporcionar soluções digitais para 
+                as mais diversas necessidades pessoais e empresariais. Além de co-CEO, 
+                atuo como Desenvolvedor na empresa.</p>
+            </div>
+            <div class="col-md-4">
+                <img class="rounded-circle" :src="img" :width=width>
+            </div>
         </div>`,
     data: function() {
         return {
             nome: "João Pedro Rios Carvalho",
-            idade: 20
+            idade: 20,
+            cidade: "Feira de Santana",
+            estado: "Bahia",
+            cursos: ['Engenharia de Computação pela Universidade Estadual de Feira de Santana(UEFS)',
+                    'Técnico em Desenvolvimento de Sistemas pelo SENAI-BA'],
+            img: "img/joao.png",
+            width: 400
         }
     }
 });
@@ -57,10 +115,6 @@ var app = new Vue({
     data: {
         currentTab: "Home",
         tabs: ["Home", "Sobre", "Contato"]
-    },
-    methods: {
-        showAbout: function () {
-        }
     },
     computed: {
         currentTabComponent() {
